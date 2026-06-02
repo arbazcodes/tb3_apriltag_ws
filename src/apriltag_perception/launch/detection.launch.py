e063d750f871794125f16ef4c819438d492a7f37
Arbaz Khan <arbazkhan5300@gmail.com>
2026-06-02 14:41:51 +0200

    fix: add camera_relay for correct coordinates covention

--- src/apriltag_perception/launch/detection.launch.py
+++ src/apriltag_perception/launch/detection.launch.py
@@ -3,22 +3,31 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     config = os.path.join(
         get_package_share_directory("apriltag_perception"),
-        "config", "apriltag.yaml",
+        "config",
+        "apriltag.yaml",
     )
-    return LaunchDescription([
-        Node(
-            package="apriltag_ros",
-            executable="apriltag_node",
-            name="apriltag_node",
-            parameters=[config],
-            
-            remappings=[
-                ("image_rect",  "/camera/image_raw"),
-                ("camera_info", "/camera/camera_info"),
-            ],
-            output="screen",
-        )
-    ])
+
+    relay = Node(
+        package="apriltag_perception",
+        executable="camera_frame_relay",
+        name="camera_frame_relay",
+        output="screen",
+    )
+
+    apriltag = Node(
+        package="apriltag_ros",
+        executable="apriltag_node",
+        name="apriltag_node",
+        parameters=[config],
+        remappings=[
+            ("image_rect", "/camera/image_raw_optical"),
+            ("camera_info", "/camera/camera_info_optical"),
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription([relay, apriltag])
