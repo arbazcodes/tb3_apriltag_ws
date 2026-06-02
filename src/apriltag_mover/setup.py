6387fe7730df1ce9187463eff886ed70d5706e0f
Arbaz Khan <arbazkhan5300@gmail.com>
2026-06-03 00:03:41 +0200

    feat: add follower package

--- src/apriltag_mover/setup.py
+++ src/apriltag_mover/setup.py
@@ -8,8 +8,7 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/config', glob('config/*.yaml')),
         ('share/' + package_name + '/launch', glob('launch/*.py')),
@@ -17,4 +16,9 @@ setup(
     install_requires=['setuptools'],
     zip_safe=True,
     tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'mover_node = apriltag_mover.mover_node:main',
+        ],
+    },
 )
